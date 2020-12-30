d099474a91a699844d9b6650d0b24da8058caf87
tedu14 <thalison.eduardo14@gmail.com>
2020-12-30 15:25:53 -0400

    feat: add a placar

---
+++ src/components/Placar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="placar">
+    <span>{{ name }}: {{ placarPlayer }}</span>
+    <span> - </span>
+    <span>Bot: {{ placarBot }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Placar",
+  props: {
+    placarPlayer: String,
+    placarBot: String,
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
+
+  beforeMount() {
+    this.name = localStorage.getItem("player:name");
+  },
+};
+</script>
+
+<style scoped>
+.placar {
+  width: 95%;
+  max-width: 480px;
+  margin: 0 auto;
+
+  padding: 0.4rem 1.2rem;
+
+  background-color: var(--color-primary-dark);
+
+  border-radius: 0.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+</style>
